01473587de1166887b7c2a8f6ac791707654225a
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-15 18:00:47 +0200

    Task 88: Rolled back README.md and LICENSE files to appear in jar files.

--- project.clj
+++ project.clj
@@ -1,18 +1,15 @@
-(defproject org.clojars.vladimirmarkovic86/mail-lib "0.1.2"
+(defproject org.clojars.vladimirmarkovic86/mail-lib "0.1.3"
   :description "Mail library"
   :url "http://github.com/VladimirMarkovic86/mail-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [javax.mail/mail "1.4.7"]
+                 [com.sun.mail/javax.mail "1.6.2"]
                  ]
 
   :min-lein-version "2.0.0"
   
   :source-paths ["src/clj"]
   :test-paths ["test/clj"]
-  :java-source-paths ["src/java"]
-  
-  :jar-exclusions [#"README.md$"
-                   #"LICENSE$"])
+  :java-source-paths ["src/java"])
 
